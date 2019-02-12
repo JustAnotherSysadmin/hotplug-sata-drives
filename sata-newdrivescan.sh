b15c549b23336f425b2112dd145b904576fd0fed
JustAnotherSysadmin <47566727+JustAnotherSysadmin@users.noreply.github.com>
2019-02-12 12:20:47 -0500

    Create sata-newdrivescan.sh

---
+++ sata-newdrivescan.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+echo "- - -" > /sys/class/scsi_host/host0/scan
+echo "- - -" > /sys/class/scsi_host/host1/scan
+echo "- - -" > /sys/class/scsi_host/host2/scan
+echo "- - -" > /sys/class/scsi_host/host3/scan
+echo "- - -" > /sys/class/scsi_host/host4/scan
+echo "- - -" > /sys/class/scsi_host/host5/scan
+echo "- - -" > /sys/class/scsi_host/host6/scan
+echo "- - -" > /sys/class/scsi_host/host7/scan
+
